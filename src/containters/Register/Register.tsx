efa36386fa8bc56462871ee7d748a6cc5522241b
Viktor <viktor.ivanov@extrawest.com>
2023-02-14 14:31:23 +0200

    Configurated firebase login and registration

--- src/containters/Register/Register.tsx
+++ src/containters/Register/Register.tsx
@@ -1,19 +1,32 @@
+import { useCallback } from "react";
 import { useIntl } from "react-intl";
 import { Link } from "react-router-dom";
 import { Box, Typography } from "@mui/material";
 import { RegisterForm } from "src/components";
-import { AppRouteEnum } from "src/types";
+import { AppRouteEnum, RegisterFormFields } from "src/types";
 import { registerStyles } from "./Register.styles";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { firebaseAuth } from "src/firebase";
+import { useHandleError } from "src/hooks";
 
 export const Register = () => {
 	const intl = useIntl();
+	const [createUserWithEmailAndPassword, , loadingRegisterViaEmail, errorRegisterViaEmail] = useCreateUserWithEmailAndPassword(firebaseAuth);
+	useHandleError(!loadingRegisterViaEmail && !!errorRegisterViaEmail, errorRegisterViaEmail?.message);
+
+	const onSubmit = useCallback(
+		({email, password}: RegisterFormFields) => {
+			createUserWithEmailAndPassword(email.trim(), password.trim());
+		},
+		[]
+	);
 
 	return (
 		<Box
 			sx={registerStyles.root}
 		>
 			<Typography variant="h3">{intl.formatMessage({ id: "registerPageTitle" })}</Typography>
-			<RegisterForm onSubmit={() => null} />
+			<RegisterForm onSubmit={onSubmit} />
 			<Link to={AppRouteEnum.LOGIN}>
 				<Typography
 					variant="subtitle1"
