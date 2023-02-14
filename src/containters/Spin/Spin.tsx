065e5ac0e615a63f1b97cecebd511a347f28b139
Viktor <viktor.ivanov@extrawest.com>
2023-02-14 19:02:20 +0200

    Configurated spin page basis

--- src/containters/Spin/Spin.tsx
+++ src/containters/Spin/Spin.tsx
@@ -1,7 +1,35 @@
+import { useIntl } from "react-intl";
+import { Box, Typography, Button } from "@mui/material";
+import { spinStyles } from "./Spin.styles";
+import Wheel from "./Wheel/Wheel";
 
 export const Spin = () => {
+	const intl = useIntl();
+
 	return (
-		<div>Spin</div>
+		<Box
+			sx={spinStyles.root}
+		>
+			<Box
+				sx={spinStyles.ribbon}
+			>
+				<Typography
+					sx={spinStyles.ribbonText}
+				>
+					{intl.formatMessage({ id: "spinPageTitle" })}
+				</Typography>
+			</Box>
+			<Wheel />
+			<Button
+				sx={spinStyles.button}
+			>
+				<Typography
+					sx={spinStyles.buttonText}
+				>
+					{intl.formatMessage({ id: "spinButtonTitle" })}
+				</Typography>
+			</Button>
+		</Box>
 	);
 };
 
---
+++ src/containters/Spin/Wheel/Wheel.tsx
@@ -0,0 +1,8 @@
+
+export const Wheel = () => {
+	return (
+		<div>Wheel</div>
+	);
+};
+
+export default Wheel;
